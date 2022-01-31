816ca5dabc84064d30111aec36639d3badacef3c
Juz-Tom-J <98319250+Juz-Tom-J@users.noreply.github.com>
2022-01-31 08:26:43 +0530

    Create Arithmetic Expression.java

---
+++ Arithmetic Expression.java
@@ -0,0 +1,31 @@
+package JavaPack;
+
+import java.util.Scanner;
+
+public class javapck_arithmetic_exception {
+
+	public static void main(String[] args) {
+		
+		int n1,d;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the no: ");
+		n1=sc.nextInt();
+         
+		try {
+      	      d = n1/0;
+        	  System.out.println("Result: "+d);
+        	  
+        	  
+        }
+         catch(ArithmeticException e1) {
+        	 throw new ArithmeticException("Division by zero");
+         }
+		  finally {
+			  System.out.println("End of Operation");
+		  }
+	}
+}
+	
+		  
