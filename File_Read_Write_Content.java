e357808e7658f062aa55859a27f4d1bb4d635c06
Juz-Tom-J <98319250+Juz-Tom-J@users.noreply.github.com>
2022-02-04 10:26:44 +0530

    Create File_Read_Write_Content.java

---
+++ File_Read_Write_Content.java
@@ -0,0 +1,39 @@
+package JavaPack;
+
+import java.io.*;
+
+public class javapack_read_write_content {
+
+	public static void main(String[] args) throws IOException {
+		File f1 = new File("content.txt");
+		
+		if(f1.exists())
+			System.out.println("File exists");
+		
+		else
+		{
+			f1.createNewFile();
+			System.out.println("File created");
+		}
+		
+		FileWriter fw = new FileWriter("content.txt",true);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter data: ");
+		String s = br.readLine();
+		fw.write(s+" ");
+		
+		System.out.println("Content added");
+		fw.close();
+		
+		FileReader fr = new FileReader("content.txt");
+		BufferedReader br1 = new BufferedReader(fr);
+		
+		String s1;
+		while((s1=br1.readLine())!=null)
+			System.out.println(s1);
+		
+		fr.close();
+	}
+
+}
