350f6b426db32d93450fd3a0d546aeadd1a97bff
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-02-13 21:14:49 +0530

    Code added

---
+++ Swap_Without_Temporary_Variable.java
@@ -0,0 +1,25 @@
+package Prog_Pack_1;
+
+import java.util.Scanner;
+
+public class prog_pack_1_swapwithouttemp {
+
+public static void main(String[] args) {
+
+int a,b,temp;
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the 1st and 2nd numbers:");
+a=sc.nextInt();
+b=sc.nextInt();
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+       
+        System.out.println("Value of 1st no. after swap: "+a);
+        System.out.println("Value of 2nd no. after swap: "+b);
+
+}
+
+}
