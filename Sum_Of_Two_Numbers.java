30ee6d56c1ba8693c8f6e978f2bf254fa6b6f489
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-02-13 21:12:30 +0530

    Code added

---
+++ Sum_Of_Two_Numbers.java
@@ -0,0 +1,24 @@
+package Prog_Pack_1;
+
+import java.util.Scanner;
+
+public class prog_pack_1_sumof2
+{
+
+public static void main(String[] args)
+{
+int a,b;
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the first number:");
+a=sc.nextInt();
+
+Scanner xc=new Scanner(System.in);
+System.out.println("Enter the second number:");
+b=xc.nextInt();
+
+System.out.println("Sum="+(a+b));
+
+}
+
+}
