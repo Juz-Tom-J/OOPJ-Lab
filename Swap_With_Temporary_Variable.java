36a8ba7dd851d5395bbe1a62f350dd521b84eb49
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-02-13 21:13:54 +0530

    Code added

---
+++ Swap_With_Temporary_Variable.java
@@ -0,0 +1,26 @@
+package Prog_Pack_1;
+
+import java.util.Scanner;
+
+public class prog_pack_1_swapwithtemp {
+
+public static void main(String[] args) {
+
+int a,b,temp;
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the 1st and 2nd numbers:");
+a=sc.nextInt();
+b=sc.nextInt();
+
+        temp=a;
+        a=b;
+        b=temp;
+       
+        System.out.println("Value of 1st no. after swap: "+a);
+        System.out.println("Value of 2nd no. after swap: "+b);
+}
+
+}
+
+
