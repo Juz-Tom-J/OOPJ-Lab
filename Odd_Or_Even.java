ae7db229c89142ec2896384aacec12ee1b7793e2
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-02-16 22:30:53 +0530

    Code added

---
+++ Odd_Or_Even.java
@@ -0,0 +1,25 @@
+package Prog_Pack_1;
+
+import java.util.Scanner;
+
+public class prog_pack_1_oddoreven {
+
+public static void main(String[] args) {
+
+int a;
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number:");
+a=sc.nextInt();
+
+if(a%2==0)
+System.out.println("The entered number is even: "+a);
+
+else
+System.out.println("The entered number is odd: "+a);
+ 
+
+}
+
+}
+
